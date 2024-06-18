9792411f093e86db55aa81c698b177db782b30cb
Ankur Kumar <ank@leoank.me>
2024-06-18 15:38:07 -0400

    Feat(experiment): Add PCP experiment schema

---
+++ starrynight/src/starrynight/experiment.py
@@ -0,0 +1,59 @@
+"""
+Schema for specifying experiment variables and generated outputs.
+"""
+
+from abc import ABC
+from enum import Enum
+from typing import Callable, Optional
+from pydantic import BaseModel, Field
+
+from starrynight.schema import MeasuredInventory
+
+
+class ImageMetadataGeneric(BaseModel):
+    name: str
+    batch_id: str
+    plate_id: str
+    site_id: str
+    img_format: str
+    assay_type: str
+    channel_dict: dict
+
+
+class ImageFrameType(Enum):
+    ROUND = "round"
+    SQUARE = "square"
+
+
+class AcqusitionOrderType(Enum):
+    SNAKE = "snake"
+    ROWS = "rows"
+
+
+class Experiment(BaseModel, ABC):
+    dataset_id: str
+    data_production_contact: Optional[str] = None
+    data_processing_contact: Optional[str] = None
+
+
+class SBSConfig(BaseModel):
+    im_per_well: int = Field(320)
+    n_cycles: int = Field(12)
+    img_overlap_pct: int = Field(10)
+    img_frame_type: ImageFrameType = Field(ImageFrameType.ROUND)
+    channel_dict: dict
+    acquisition_order: AcqusitionOrderType = Field(AcqusitionOrderType.SNAKE)
+
+
+class CPConfig(BaseModel):
+    im_per_well: int = Field(1364)
+    img_overlap_pct: int = Field(10)
+    img_frame_type: ImageFrameType = Field(ImageFrameType.ROUND)
+    channel_dict: dict
+    acquisition_order: AcqusitionOrderType = Field(AcqusitionOrderType.SNAKE)
+
+
+class PCPGeneric(Experiment):
+    path_parser: Callable[[str], MeasuredInventory]
+    sbs_config: SBSConfig
+    cp_config: CPConfig
