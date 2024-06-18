a3ae38958ff5d70e5c928671c47dcaf1c6d2ec74
Ankur Kumar <ank@leoank.me>
2024-06-18 15:37:34 -0400

    Feat(schema): Add schema module

---
+++ starrynight/src/starrynight/schema.py
@@ -0,0 +1,75 @@
+"""
+Schema module.
+"""
+
+from typing import Annotated, List, Optional
+
+from pydantic import Field, computed_field, ConfigDict, BeforeValidator
+from cpgdata.parser import ParsedPrefix, WorkspaceFolder
+from cpgdata.measurement import get_is_dir, get_key_parts
+
+
+class MeasuredInventory(ParsedPrefix):
+    is_parsing_error: bool = Field(default=False)
+    errors: Optional[str] = Field(default=None)
+    is_dir: Annotated[bool, BeforeValidator(get_is_dir)] = Field(
+        validation_alias="key", default=False
+    )
+    key_parts: Annotated[
+        List[str],
+        BeforeValidator(get_key_parts),
+    ] = Field(validation_alias="key", default=[])
+
+    # Model configuration
+    model_config = ConfigDict(populate_by_name=True)
+
+    @computed_field(return_type=Optional[str])
+    @property
+    def workspace_dir(self: "MeasuredInventory") -> Optional[str]:
+        """Computed workspace directory value.
+
+        Returns
+        -------
+        Optional[str]
+            Computed workspace directory value.
+
+        """
+        workspace_dir = self.workspace_root_dir
+        workspace_dir = str(workspace_dir).split("/")[0]
+        return WorkspaceFolder(workspace_dir).value
+
+    def get_all_fields(self: "MeasuredInventory") -> dict:
+        """Get all the fields of the model.
+
+        Returns
+        -------
+        dict
+            A dict with field key and FieldInfo or ComputedFieldInfo.
+
+        """
+        model_fields = self.model_fields
+        return {**model_fields, **self.model_computed_fields}
+
+    @staticmethod
+    def gen_error_entry(obj_key: str, e: Exception) -> "MeasuredInventory":
+        """Generate an error entry for the parsed object.
+
+        Parameters
+        ----------
+        obj_key : str
+            Object key.
+        e : Exception
+            Pydantic validation error.
+
+        Returns
+        -------
+        "MeasuredPrefix"
+           Validation error as a parsed object.
+
+        """
+        print(f"I am called with the error {e}")
+        return MeasuredInventory.model_construct(
+            obj_key=obj_key,
+            is_parsing_error=True,
+            errors=str(e),
+        )
