ab89cce142dbabfb18a59975f23d47893cf3944b
Ankur Kumar <ank@leoank.me>
2025-02-11 17:22:52 -0500

    update(starrynight): example notebook

--- starrynight/notebooks/create_index.py
+++ starrynight/notebooks/create_index.py
@@ -4,27 +4,35 @@ from pathlib import Path
 
 from pipecraft.backend.snakemake import SnakeMakeBackend, SnakeMakeConfig
 
-from starrynight.experiments.common import DummyExperiment
+from starrynight.experiments.pcp_generic import PCPGeneric, PCPGenericInitConfig
 from starrynight.modules.gen_inv import GenInvModule
 from starrynight.pipelines.index import create_index_pipeline
+from starrynight.pipelines.pcp_generic import create_pcp_generic_pipeline
 from starrynight.schema import DataConfig
 
 # Setup experiment
 data = DataConfig(
     dataset_path=Path("/datastore/cpg0999-merck-asma"),
-    storage_path=Path("./run001/workspace"),
+    storage_path=Path("/datastore/cpg0999-merck-asma"),
     workspace_path=Path("./run001/workspace"),
 )
-experiment = DummyExperiment(dataset_id="cpg0999-merck-asma")
+
+pcp_exp = PCPGeneric.from_index(
+    data.storage_path.joinpath("index/index.parquet"),
+    PCPGenericInitConfig(
+        barcode_csv_path=data.storage_path.joinpath("barcode.csv")
+    ).model_dump(),
+)
 
 # Create the pipeline
-pipeline = create_index_pipeline(experiment, data)
+# modules, simple_pipeline = create_index_pipeline(data)
+modules, simple_pipeline = create_pcp_generic_pipeline(data, experiment=pcp_exp)
 
 
 # Configure execution backend
 config = SnakeMakeConfig()
-exec_backend = SnakeMakeBackend(
-    pipeline, config, data.storage_path, data.workspace_path
+exec_backend01 = SnakeMakeBackend(
+    simple_pipeline, config, data.storage_path, data.workspace_path
 )
 
 # Compile to get the generated snakemake file if
@@ -39,13 +47,17 @@ exec_backend = SnakeMakeBackend(
 # -------------------------------------------------------------------
 
 # Iniialize module with experiment and data
-gen_inv_module = GenInvModule.from_config(experiment, data)
+gen_inv_module = GenInvModule.from_config(data)
 
 # Inspect current specification and make changes
 print(gen_inv_module.spec)
 gen_inv_module.spec.inputs[0].path = Path("path/to/my/parser.lark").resolve().__str__()
 
 # Add the configured module back to pipeline
-pipeline = create_index_pipeline(
-    experiment, data, {GenInvModule.uid(): gen_inv_module.spec}
+configured_modules, configure_pipeline = create_index_pipeline(
+    data, updated_spec_dict={GenInvModule.uid(): gen_inv_module.spec}
 )
+
+# --------------------------------------------------------------------
+# Experiment configuration.
+# --------------------------------------------------------------------
