68aeebb802c7f55e43b4047e68118bae3e5c4620
Shantanu Singh <shsingh@broadinstitute.org>
2025-04-04 14:29:15 -0400

    doc(starrynight): adapt pipelines to replace RunCellpose  with IdentifyPrimaryObjects, and drop cycles 4-10

--- docs/tester/assets/pcpip-run-cellprofiler/run_all.sh
+++ docs/tester/assets/pcpip-run-cellprofiler/run_all.sh
@@ -1,6 +1,7 @@
 STARRYNIGHT_REPO_REL="../../../.."
 LOAD_DATA_DIR="${STARRYNIGHT_REPO_REL}/scratch/starrynight_example_output_baseline/Source1/workspace/load_data_csv/Batch1/Plate1_trimmed"
 REPRODUCE_DIR="${STARRYNIGHT_REPO_REL}/scratch/reproduce_starrynight_example_output_baseline"
+METADATA_DIR="${STARRYNIGHT_REPO_REL}/scratch/starrynight_example_input/Source1/workspace/metadata"
 
 PIPELINE_DIR="../pcpip-pipelines"
 
@@ -30,17 +31,7 @@ cellprofiler \
     -L 10 \
     --pipeline ${PIPELINE_DIR}/3_CP_SegmentationCheck/ref_3_CP_SegmentationCheck.cppipe \
     --data-file ${LOAD_DATA_DIR}/load_data_pipeline3.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_segmentation/ \
-    --plugins-directory ${STARRYNIGHT_REPO_REL}/plugins/CellProfiler-plugins/active_plugins/
-
-# [CP - ERROR] cellprofiler_core.pipeline._pipeline::run_with_yield: Error detected during run of module RunCellpose
-# Traceback (most recent call last):
-#   File "/nix/store/g96aiz9nakj9h2n5il8rxb08fjf8w7gm-python3-3.11.10-env/lib/python3.11/site-packages/cellprofiler_core/pipeline/_pipeline.py", line 1080, in run_with_yield
-#     self.run_module(module, workspace)
-#   File "/nix/store/g96aiz9nakj9h2n5il8rxb08fjf8w7gm-python3-3.11.10-env/lib/python3.11/site-packages/cellprofiler_core/pipeline/_pipeline.py", line 1459, in run_module
-#     module.run(workspace)
-#   File "/Users/shsingh/Documents/GitHub/starrynight/docs/tester/assets/pcpip-run-cellprofiler/../../../../plugins/CellProfiler-plugins/active_plugins/runcellpose.py", line 508, in run
-#     from cellpose import models, io, core, utils
+    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_segmentation/
 
 # 5_BC_Illum
 
@@ -59,3 +50,14 @@ cellprofiler \
     --pipeline ${PIPELINE_DIR}/6_BC_Apply_Illum/ref_6_BC_Apply_Illum.cppipe \
     --data-file ${LOAD_DATA_DIR}/load_data_pipeline6.csv \
     --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_aligned/barcoding/
+
+# 7_BC_Preprocess
+
+cellprofiler \
+    -c \
+    -L 10 \
+    -i ${METADATA_DIR}/ \
+    --pipeline ${PIPELINE_DIR}/7_BC_Preprocess/ref_7_BC_Preprocess.cppipe \
+    --data-file ${LOAD_DATA_DIR}/load_data_pipeline7.csv \
+    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_preprocessed/barcoding/ \
+    --plugins-directory ${STARRYNIGHT_REPO_REL}/plugins/CellProfiler-plugins/active_plugins/
