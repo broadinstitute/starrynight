474ba0573f7483a42b117fb38cdf5b11b8579499
Shantanu Singh <shsingh@broadinstitute.org>
2025-04-05 07:32:53 -0400

    doc(starrynight): script to run all pipelines

--- docs/tester/assets/pcpip-run-cellprofiler/run_all.sh
+++ docs/tester/assets/pcpip-run-cellprofiler/run_all.sh
@@ -5,71 +5,199 @@ METADATA_DIR="${STARRYNIGHT_REPO_REL}/scratch/starrynight_example_input/Source1/
 
 PIPELINE_DIR="../pcpip-pipelines"
 
+PLATE=Plate1
+WELLS=("WellA1" "WellA2" "WellB1")
+SITES=(0 1)
+SBSCYCLES=(1 2 3)
 
-# 1_CP_Illum
+# Define all pipeline configurations
+declare -A PIPELINE_CONFIG=(
+  # Pipeline file names
+  [1,file]="ref_1_CP_Illum.cppipe"
+  [2,file]="ref_2_CP_Apply_Illum.cppipe"
+  [3,file]="ref_3_CP_SegmentationCheck.cppipe"
+  [5,file]="ref_5_BC_Illum.cppipe"
+  [6,file]="ref_6_BC_Apply_Illum.cppipe"
+  [7,file]="ref_7_BC_Preprocess.cppipe"
+  [9,file]="ref_9_Analysis.cppipe"
 
-cellprofiler \
-    -c \
-    -L 10 \
-    --pipeline ${PIPELINE_DIR}/1_CP_Illum/ref_1_CP_Illum.cppipe \
-    --data-file ${LOAD_DATA_DIR}/load_data_pipeline1.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/illum/Plate1
+  # Data files
+  [1,data]="load_data_pipeline1.csv"
+  [2,data]="load_data_pipeline2.csv"
+  [3,data]="load_data_pipeline3.csv"
+  [5,data]="load_data_pipeline5.csv"
+  [6,data]="load_data_pipeline6.csv"
+  [7,data]="load_data_pipeline7.csv"
+  [9,data]="load_data_pipeline9.csv"
 
-# 2_CP_Apply_Illum
+  # Output directory patterns
+  [1,output]="illum/PLATE"
+  [2,output]="images_corrected/painting/PLATE-WELL"
+  [3,output]="images_segmentation/PLATE-WELL"
+  [5,output]="illum/PLATE"
+  [6,output]="images_aligned/barcoding/PLATE-WELL-SITE"
+  [7,output]="images_corrected/barcoding/PLATE-WELL-SITE"
+  [9,output]="workspace/analysis/Batch1/PLATE-WELL-SITE"
 
-cellprofiler \
-    -c \
-    -L 10 \
-    --pipeline ${PIPELINE_DIR}/2_CP_Apply_Illum/ref_2_CP_Apply_Illum.cppipe \
-    --data-file ${LOAD_DATA_DIR}/load_data_pipeline2.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_corrected/painting/
+  # Group patterns
+  [1,group]="Metadata_Plate=PLATE"
+  [2,group]="Metadata_Plate=PLATE,Metadata_Well=WELL"
+  [3,group]="Metadata_Plate=PLATE,Metadata_Well=WELL"
+  [5,group]="Metadata_Plate=PLATE,Metadata_SBSCycle=SBSCYCLE"
+  [6,group]="Metadata_Plate=PLATE,Metadata_Well=WELL,Metadata_Site=SITE"
+  [7,group]="Metadata_Plate=PLATE,Metadata_Well=WELL,Metadata_Site=SITE"
+  [9,group]="Metadata_Plate=PLATE,Metadata_Well=WELL,Metadata_Site=SITE"
 
-# 3_CP_SegmentationCheck
+  # Required parameters (comma-separated)
+  [1,params]="PLATE"
+  [2,params]="PLATE,WELL"
+  [3,params]="PLATE,WELL"
+  [5,params]="PLATE,SBSCYCLE"
+  [6,params]="PLATE,WELL,SITE"
+  [7,params]="PLATE,WELL,SITE"
+  [9,params]="PLATE,WELL,SITE"
 
-cellprofiler \
-    -c \
-    -L 10 \
-    --pipeline ${PIPELINE_DIR}/3_CP_SegmentationCheck/ref_3_CP_SegmentationCheck.cppipe \
-    --data-file ${LOAD_DATA_DIR}/load_data_pipeline3.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_segmentation/
+  # Needs metadata flag (true/false)
+  [1,metadata]="false"
+  [2,metadata]="false"
+  [3,metadata]="false"
+  [5,metadata]="false"
+  [6,metadata]="false"
+  [7,metadata]="true"
+  [9,metadata]="true"
 
-# 5_BC_Illum
+  # Run in background (true/false)
+  [1,background]="false"
+  [2,background]="true"
+  [3,background]="true"
+  [5,background]="true"
+  [6,background]="true"
+  [7,background]="true"
+  [9,background]="true"
 
-cellprofiler \
-    -c \
-    -L 10 \
-    --pipeline ${PIPELINE_DIR}/5_BC_Illum/ref_5_BC_Illum.cppipe \
-    --data-file ${LOAD_DATA_DIR}/load_data_pipeline5.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/illum/Plate1
+  # Needs plugins (true/false)
+  [1,plugins]="false"
+  [2,plugins]="false"
+  [3,plugins]="false"
+  [5,plugins]="false"
+  [6,plugins]="false"
+  [7,plugins]="true"
+  [9,plugins]="true"
+)
 
-# 6_BC_Apply_Illum
+# Function to apply variable substitution to a pattern
+apply_pattern() {
+  local pattern=$1
+  local result=$pattern
 
-cellprofiler \
-    -c \
-    -L 10 \
-    --pipeline ${PIPELINE_DIR}/6_BC_Apply_Illum/ref_6_BC_Apply_Illum.cppipe \
-    --data-file ${LOAD_DATA_DIR}/load_data_pipeline6.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_aligned/barcoding/
+  # Apply all available substitutions
+  result=${result//PLATE/$PLATE}
 
-# 7_BC_Preprocess
+  if [[ -n "$WELL" ]]; then
+    result=${result//WELL/$WELL}
+  fi
 
-cellprofiler \
-    -c \
-    -L 10 \
-    -i ${METADATA_DIR}/ \
-    --pipeline ${PIPELINE_DIR}/7_BC_Preprocess/ref_7_BC_Preprocess.cppipe \
-    --data-file ${LOAD_DATA_DIR}/load_data_pipeline7.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/images_preprocessed/barcoding/ \
-    --plugins-directory ${STARRYNIGHT_REPO_REL}/plugins/CellProfiler-plugins/active_plugins/
+  if [[ -n "$SITE" ]]; then
+    result=${result//SITE/$SITE}
+  fi
+
+  if [[ -n "$SBSCYCLE" ]]; then
+    result=${result//SBSCYCLE/$SBSCYCLE}
+  fi
+
+  echo "$result"
+}
+
+# Function to run a pipeline with the right parameters
+run_pipeline() {
+  local pipeline=$1
+  local required_params=${PIPELINE_CONFIG[$pipeline,params]}
+  local use_metadata=${PIPELINE_CONFIG[$pipeline,metadata]}
+  local run_background=${PIPELINE_CONFIG[$pipeline,background]}
+  local use_plugins=${PIPELINE_CONFIG[$pipeline,plugins]}
+
+  # Build the basic command
+  local cmd="cellprofiler -c -L 10"
+
+  # Add group parameter
+  cmd+=" -g \"$(apply_pattern "${PIPELINE_CONFIG[$pipeline,group]}")\""
+
+  # Add metadata path if needed
+  if [[ "$use_metadata" == "true" ]]; then
+    cmd+=" -i ${METADATA_DIR}/"
+  fi
+
+  # Add pipeline, data file and output directory
+  cmd+=" --pipeline ${PIPELINE_DIR}/${PIPELINE_CONFIG[$pipeline,file]}"
+  cmd+=" --data-file ${LOAD_DATA_DIR}/${PIPELINE_CONFIG[$pipeline,data]}"
+  cmd+=" --output-directory $(apply_pattern "${REPRODUCE_DIR}/Source1/Batch1/${PIPELINE_CONFIG[$pipeline,output]}")"
+
+  # Add plugins if needed
+  if [[ "$use_plugins" == "true" ]]; then
+    cmd+=" --plugins-directory ${STARRYNIGHT_REPO_REL}/plugins/CellProfiler-plugins/active_plugins/"
+  fi
+
+  # Add background processing if needed
+  if [[ "$run_background" == "true" ]]; then
+    cmd+=" &"
+  fi
+
+  # Execute the command
+  eval "$cmd"
+}
+
+# 9_Analysis needs this directory
+mkdir -p ${REPRODUCE_DIR}/Source1/workspace/analysis/Batch1/
+
+# 1_CP_Illum - PLATE only
+PIPELINE=1
+run_pipeline $PIPELINE
+
+# 2_CP_Apply_Illum - PLATE, WELL
+PIPELINE=2
+for WELL in "${WELLS[@]}"; do
+    run_pipeline $PIPELINE
+done
+wait
+
+# 3_CP_SegmentationCheck - PLATE, WELL
+PIPELINE=3
+for WELL in "${WELLS[@]}"; do
+    run_pipeline $PIPELINE
+done
+wait
 
 
-# 9_Analysis
+# 5_BC_Illum - PLATE, SBSCYCLE
+PIPELINE=5
+for SBSCYCLE in "${SBSCYCLES[@]}"; do
+    run_pipeline $PIPELINE
+done
+wait
 
-cellprofiler \
-    -c \
-    -L 10 \
-    -i ${METADATA_DIR}/ \
-    --pipeline ${PIPELINE_DIR}/9_Analysis/ref_9_Analysis.cppipe \
-    --data-file ${LOAD_DATA_DIR}/load_data_pipeline9.csv \
-    --output-directory ${REPRODUCE_DIR}/Source1/Batch1/analysis/ \
-    --plugins-directory ${STARRYNIGHT_REPO_REL}/plugins/CellProfiler-plugins/active_plugins/
+# 6_BC_Apply_Illum - PLATE, WELL, SITE
+PIPELINE=6
+for WELL in "${WELLS[@]}"; do
+    for SITE in "${SITES[@]}"; do
+        run_pipeline $PIPELINE
+    done
+done
+wait
+
+# 7_BC_Preprocess - PLATE, WELL, SITE
+PIPELINE=7
+for WELL in "${WELLS[@]}"; do
+    for SITE in "${SITES[@]}"; do
+        run_pipeline $PIPELINE
+    done
+done
+wait
+
+# 9_Analysis - PLATE, WELL, SITE
+PIPELINE=9
+for WELL in "${WELLS[@]}"; do
+    for SITE in "${SITES[@]}"; do
+        run_pipeline $PIPELINE
+    done
+done
+wait
