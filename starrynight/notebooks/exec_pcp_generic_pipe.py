d5be44ed3717fc9cec0b15454e14a8d80b5e8d6b
Shantanu Singh <shsingh@broadinstitute.org>
2025-04-10 18:13:24 -0400

    fix(starrynight): comment out unused imports for now

--- starrynight/notebooks/exec_pcp_generic_pipe.py
+++ starrynight/notebooks/exec_pcp_generic_pipe.py
@@ -1,4 +1,4 @@
-"""Execute PCP generic pipeline"""
+"""Execute PCP generic pipeline."""
 
 # %%
 from pathlib import Path
@@ -6,10 +6,11 @@ from pathlib import Path
 from pipecraft.backend.snakemake import SnakeMakeBackend, SnakeMakeConfig
 
 from starrynight.experiments.pcp_generic import PCPGeneric, PCPGenericInitConfig
-from starrynight.modules import cp_pre_segcheck
-from starrynight.modules.analysis.analysis_cp import AnalysisInvokeCPModule
-from starrynight.modules.analysis.analysis_cppipe import AnalysisGenCPPipeModule
-from starrynight.modules.analysis.analysis_load_data import AnalysisGenLoadDataModule
+
+# from starrynight.modules import cp_pre_segcheck
+# from starrynight.modules.analysis.analysis_cp import AnalysisInvokeCPModule
+# from starrynight.modules.analysis.analysis_cppipe import AnalysisGenCPPipeModule
+# from starrynight.modules.analysis.analysis_load_data import AnalysisGenLoadDataModule
 from starrynight.modules.cp_illum_apply.apply_cp import CPApplyIllumInvokeCPModule
 from starrynight.modules.cp_illum_apply.apply_cppipe import CPApplyIllumGenCPPipeModule
 from starrynight.modules.cp_illum_apply.apply_load_data import (
@@ -22,15 +23,16 @@ from starrynight.modules.cp_illum_calc.calc_cppipe import (
 from starrynight.modules.cp_illum_calc.calc_load_data import (
     CPCalcIllumGenLoadDataModule,
 )
-from starrynight.modules.cp_pre_segcheck.pre_segcheck_cp import (
-    CPPreSegcheckInvokeCPModule,
-)
-from starrynight.modules.cp_pre_segcheck.pre_segcheck_cppipe import (
-    CPPreSegcheckGenCPPipeModule,
-)
-from starrynight.modules.cp_pre_segcheck.pre_segcheck_load_data import (
-    CPPreSegcheckGenLoadDataModule,
-)
+
+# from starrynight.modules.cp_pre_segcheck.pre_segcheck_cp import (
+#     CPPreSegcheckInvokeCPModule,
+# )
+# from starrynight.modules.cp_pre_segcheck.pre_segcheck_cppipe import (
+#     CPPreSegcheckGenCPPipeModule,
+# )
+# from starrynight.modules.cp_pre_segcheck.pre_segcheck_load_data import (
+#     CPPreSegcheckGenLoadDataModule,
+# )
 from starrynight.modules.cp_segcheck.segcheck_cp import (
     CPSegcheckInvokeCPModule,
 )
@@ -42,28 +44,29 @@ from starrynight.modules.cp_segcheck.segcheck_load_data import (
 )
 from starrynight.modules.gen_index import GenIndexModule
 from starrynight.modules.gen_inv import GenInvModule
-from starrynight.modules.sbs_align.algin_cp import SBSAlignInvokeCPModule
-from starrynight.modules.sbs_align.algin_cppipe import SBSAlignGenCPPipeModule
-from starrynight.modules.sbs_align.algin_load_data import SBSAlignGenLoadDataModule
-from starrynight.modules.sbs_illum_apply.apply_cp import SBSApplyIllumInvokeCPModule
-from starrynight.modules.sbs_illum_apply.apply_cppipe import (
-    SBSApplyIllumGenCPPipeModule,
-)
-from starrynight.modules.sbs_illum_apply.apply_load_data import (
-    SBSApplyIllumGenLoadDataModule,
-)
-from starrynight.modules.sbs_illum_calc.calc_cp import SBSCalcIllumInvokeCPModule
-from starrynight.modules.sbs_illum_calc.calc_cppipe import SBSCalcIllumGenCPPipeModule
-from starrynight.modules.sbs_illum_calc.calc_load_data import (
-    SBSCalcIllumGenLoadDataModule,
-)
-from starrynight.modules.sbs_preprocess.preprocess_cp import SBSPreprocessInvokeCPModule
-from starrynight.modules.sbs_preprocess.preprocess_cppipe import (
-    SBSPreprocessGenCPPipeModule,
-)
-from starrynight.modules.sbs_preprocess.preprocess_load_data import (
-    SBSPreprocessGenLoadDataModule,
-)
+
+# from starrynight.modules.sbs_align.algin_cp import SBSAlignInvokeCPModule
+# from starrynight.modules.sbs_align.algin_cppipe import SBSAlignGenCPPipeModule
+# from starrynight.modules.sbs_align.algin_load_data import SBSAlignGenLoadDataModule
+# from starrynight.modules.sbs_illum_apply.apply_cp import SBSApplyIllumInvokeCPModule
+# from starrynight.modules.sbs_illum_apply.apply_cppipe import (
+#     SBSApplyIllumGenCPPipeModule,
+# )
+# from starrynight.modules.sbs_illum_apply.apply_load_data import (
+#     SBSApplyIllumGenLoadDataModule,
+# )
+# from starrynight.modules.sbs_illum_calc.calc_cp import SBSCalcIllumInvokeCPModule
+# from starrynight.modules.sbs_illum_calc.calc_cppipe import SBSCalcIllumGenCPPipeModule
+# from starrynight.modules.sbs_illum_calc.calc_load_data import (
+#     SBSCalcIllumGenLoadDataModule,
+# )
+# from starrynight.modules.sbs_preprocess.preprocess_cp import SBSPreprocessInvokeCPModule  # noqa: E501
+# from starrynight.modules.sbs_preprocess.preprocess_cppipe import (
+#     SBSPreprocessGenCPPipeModule,
+# )
+# from starrynight.modules.sbs_preprocess.preprocess_load_data import (
+#     SBSPreprocessGenLoadDataModule,
+# )
 from starrynight.schema import DataConfig
 
 # %% [markdown]
