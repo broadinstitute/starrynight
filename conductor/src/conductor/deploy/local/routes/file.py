4b41e492c8e2fa312d4028f115f30e294574fcb5
Ankur Kumar <ank@leoank.me>
2025-06-18 09:25:02 -0400

    feat(conductor): add file upload api

--- conductor/src/conductor/deploy/local/routes/file.py
+++ conductor/src/conductor/deploy/local/routes/file.py
@@ -1,14 +1,21 @@
 """File streaming routes."""
 
-from pathlib import Path
-
-from fastapi import APIRouter
+from cloudpathlib import AnyPath
+from fastapi import APIRouter, UploadFile
 from fastapi.responses import FileResponse
 
 file_router = APIRouter(prefix="/file", tags=["file"])
 
 
 @file_router.get("/")
-def get_file(file_path: str) -> FileResponse:
+async def get_file(file_path: str) -> FileResponse:
     """Get file."""
-    return FileResponse(Path(file_path).absolute().__str__())
+    return FileResponse(AnyPath(file_path).absolute().__str__())
+
+
+@file_router.post("/")
+async def post_file(file_path: str, file: UploadFile) -> dict:
+    """Post file."""
+    # TODO: Hack for now. Solve it properly later
+    AnyPath(file_path).write_bytes(file.file.read())
+    return {"msg": "File uploaded successfully"}
